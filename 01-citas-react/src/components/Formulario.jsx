2b866ee8c08dbc7c2c8658dee3b79a371f43527f
Carlos Ramirez <karlossander92@gmail.com>
2022-06-24 14:19:38 -0500

    Se esta haciendo el diseño con tailwind css estamos haciendo el de listado de pacientes

--- 01-citas-react/src/components/Formulario.jsx
+++ 01-citas-react/src/components/Formulario.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 const Formulario = () => {
   return (
-    <div>Formulario</div>
-  )
-}
+    <div className="w-1/2 lg:w-3/5">
+    <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+    <p className="text-lg mt-5 text-center mb-10">Añade Pacientes y {' '}<span className="text-indigo-600 font-bold">Administralos</span></p>
+    <form action="" className="bg-white shadow-md rounded-lg py-10 px-5">
+      <div className="mb-5">
+        <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold" >Nombre Mascota</label>
+        <input id="mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="text" placeholder="Nombre de la mascota" />
+      </div>
 
-export default Formulario
+      <div className="mb-5">
+        <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold" >Nombre Propietario</label>
+        <input id="propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="text" placeholder="Nombre del propietario" />
+      </div>
+
+      <div className="mb-5">
+        <label htmlFor="email" className="block text-gray-700 uppercase font-bold" >Email</label>
+        <input id="email" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="email" placeholder="Email contacto propietario" />
+      </div>
+
+      <div className="mb-5">
+        <label htmlFor="alta" className="block text-gray-700 uppercase font-bold" >Salida</label>
+        <input id="alta" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="date" />
+      </div>
+
+      <div className="mb-5">
+        <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold" >Sintomas</label>
+        <textarea id="sintomas" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Describe los sintomas"/>
+      </div>
+
+      <input type="submit" className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 rounded cursor-pointer transition-colors" value="Agregar Paciente"/>
+    </form>
+</div>
+    )
+  }
+  
+  export default Formulario
--- 01-citas-react/src/components/ListadoPacientes.jsx
+++ 01-citas-react/src/components/ListadoPacientes.jsx
@@ -2,9 +2,26 @@
 const ListadoPacientes = () => {
   return (
     <>
-        <h2>Listado Pacientes</h2>
+        <div className='md:w-1/2 lg:2/5'>
+          <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
+          <p className="text-xl mt-5 text-center mb-10">Administra tus {''} <span className="text-indigo-600 font-bold">Pacientes y Citas</span></p>
+
+          <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{' '}<span className="font-normal normal-case">Juan</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario:{' '}<span className="font-normal normal-case">Carlos Andres</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email:{' '}<span className="font-normal normal-case">correo@correo.com</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Salida:{' '}<span className="font-normal normal-case">10/12/12</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas:{' '}<span className="font-normal normal-case">Tiene gripa</span></p>
+
+          </div>
+        </div>
     </>
   )
 }
 
 export default ListadoPacientes
+
