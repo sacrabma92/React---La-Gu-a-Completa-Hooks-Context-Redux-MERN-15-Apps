1fe7f66fe9f0847b8dd50ef97f9585089f455409
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-04-18 15:11:22 -0500

    Agregando Contenido Principal

--- 04_Calculadora_Propinas/src/App.tsx
+++ 04_Calculadora_Propinas/src/App.tsx
@@ -1,9 +1,24 @@
+import { menuItems } from "./data/db"
 
 function App() {
 
+  console.log(menuItems)
+
   return (
     <>
-    <h1 className="text-8xl">Hola Mundo</h1>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
