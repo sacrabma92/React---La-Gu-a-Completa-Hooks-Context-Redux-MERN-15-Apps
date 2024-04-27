3a001a95c70984eb9e09219e6af2a7e18eac3fcd
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-04-27 11:08:38 -0500

    Mostrando la información del Paciente

--- 07-pacientes-zustand/src/components/PatientsDetails.tsx
+++ 07-pacientes-zustand/src/components/PatientsDetails.tsx
@@ -1,4 +1,5 @@
 import { Patient } from "../types"
+import PatientDetailItem from "./PatientDetailItem"
 
 type PatietnsDetailsProps = {
   patient: Patient
@@ -6,8 +7,13 @@ type PatietnsDetailsProps = {
 
 export default function PatientsDetails({ patient }: PatietnsDetailsProps) {
   return (
-    <div>
-      {patient.name}
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+      <PatientDetailItem label='ID' data={patient.id} />
+      <PatientDetailItem label='Nombre' data={patient.name} />
+      <PatientDetailItem label='Propietario' data={patient.caretaker} />
+      <PatientDetailItem label='Email' data={patient.email} />
+      <PatientDetailItem label='Fecha Alta' data={patient.fecha.toString()} />
+      <PatientDetailItem label='Sintomas' data={patient.symptoms} />
     </div>
   )
 }
