671cb1840a6fe4dc9bbeebf1c51954b8fba77d81
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-04-28 12:50:16 -0500

    Segundo llamado hacia la API para obtener el Clima

--- 08-clima-react-typescript/src/hooks/useWeather.ts
+++ 08-clima-react-typescript/src/hooks/useWeather.ts
@@ -8,7 +8,15 @@ export default function useWeather(){
     try {
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
       const {data} = await axios(geoUrl)
-      console.log(data)
+
+      const lat = data[0].lat
+      const lon = data[0].lon
+
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+      const { data: weatherResult } = await axios(weatherUrl)
+
+      console.log(weatherResult)
+
     } catch (error) {
       console.log(error)
     }
