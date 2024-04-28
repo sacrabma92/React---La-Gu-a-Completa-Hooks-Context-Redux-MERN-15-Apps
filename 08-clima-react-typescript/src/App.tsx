3fb05a13163fe0ad6b8e4f65f84f1efd1e9ea9a3
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-04-28 08:13:02 -0500

    Introducción a CSS Modules

--- 08-clima-react-typescript/src/App.tsx
+++ 08-clima-react-typescript/src/App.tsx
@@ -1,8 +1,14 @@
+import styles from "./App.module.css"
 
 export default function App() {
   return (
     <div>
-      <h1>Buscador de Clima</h1>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+
+      <div className={styles.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
     </div>
   )
 }
