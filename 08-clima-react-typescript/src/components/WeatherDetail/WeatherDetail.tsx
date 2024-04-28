025768fad32e8e4297475ed7a3e3f4a9d236a134
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-04-28 15:54:49 -0500

    Finalizando el Componente de Clima

--- 08-clima-react-typescript/src/components/WeatherDetail/WeatherDetail.tsx
+++ 08-clima-react-typescript/src/components/WeatherDetail/WeatherDetail.tsx
@@ -1,4 +1,4 @@
-// import styles from './WeatherDetail.module.css'
+import styles from './WeatherDetail.module.css'
 
 import { formatTemperature } from "../../helpers"
 import { Weather } from "../../hooks/useWeather"
@@ -9,10 +9,10 @@ type WeatherDetailsProps = {
 
 export default function WeatherDetail({weather}:WeatherDetailsProps) {
   return (
-    <div>
+    <div className={styles.container}>
       <h2>Clima de: {weather.name}</h2>
-      <p>{formatTemperature(weather.main.temp)}&deg;C</p>
-      <div>
+      <p className={styles.current}>{formatTemperature(weather.main.temp)}&deg;C</p>
+      <div className={styles.temperatures}>
         <p>Min: <span>{formatTemperature(weather.main.temp_min)}&deg;C</span></p>
         <p>Max: <span>{formatTemperature(weather.main.temp_max)}&deg;C</span></p>
       </div>
