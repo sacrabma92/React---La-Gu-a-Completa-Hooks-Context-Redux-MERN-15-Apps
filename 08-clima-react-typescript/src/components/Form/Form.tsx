66da7388da8b1cf1cb16fa6ae0177201c74cc332
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-04-28 11:25:58 -0500

    Validando el Formulario

--- 08-clima-react-typescript/src/components/Form/Form.tsx
+++ 08-clima-react-typescript/src/components/Form/Form.tsx
@@ -1,7 +1,8 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { countries } from "../../data/countries";
 import styles from './Form.module.css'
 import { SearchType } from "../../types";
+import Alert from "../Alert/Alert";
 
 export default function Form() {
 
@@ -9,6 +10,7 @@ export default function Form() {
     city:'',
     country: ''
   })
+  const [alert, setAlert] = useState('')
 
   const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
     setSearch({
@@ -17,8 +19,18 @@ export default function Form() {
     })
   }
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if(Object.values(search).includes('')){
+      setAlert('Todos los campos son obligatorios')
+      return
+    }
+  }
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
+      {alert && <Alert>{alert}</Alert>}
       <div className={styles.field}>
         <label htmlFor="city">Ciudad:</label>
         <input type="text" id="city" name="city" placeholder="Ciudad" value={search.city} onChange={handleChange}/>
