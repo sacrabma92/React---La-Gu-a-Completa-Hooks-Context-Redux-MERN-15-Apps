fbe92143b3410002fdf25ab0ee55d26121043023
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-05-03 10:59:24 -0500

    Creando State para Moneda y Criptomoneda a Cotizar

--- 09-Cripto-React-TypeScript/src/components/CriptoSearchForm.tsx
+++ 09-Cripto-React-TypeScript/src/components/CriptoSearchForm.tsx
@@ -1,18 +1,30 @@
 import { useCrypeStore } from "../store"
 import { currencies } from "../data"
+import { useState } from "react"
+import { Pair } from "../types"
 
 
 
 export default function CriptoSearchForm() {
 
   const cryptocurrencies = useCrypeStore((state) => state.cryptocurrencies)
+  const [pair, setPair] = useState<Pair>({
+    currency: '',
+    criptoCurrency: ''
+  })
 
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setPair({
+      ...pair,
+      [e.target.name]: e.target.value
+    })
+  }
 
   return (
     <form className="form">
       <div className="field">
         <label htmlFor="currency">Moneda:</label>
-        <select name="currency" id="currenct">
+        <select name="currency" id="currenct" onChange={handleChange}>
           <option value="">--Seleccione--</option>
           {currencies.map(currency => (
             <option value={currency.code} key={currency.code}>{currency.name}</option>
@@ -22,12 +34,12 @@ export default function CriptoSearchForm() {
 
       <div className="field">
         <label htmlFor="criptocurrency">Criptomoneda:</label>
-        <select name="criptocurrency" id="criptocurrency">
+        <select name="criptocurrency" id="criptocurrency" onChange={handleChange}>
           <option value="">--Seleccione--</option>
           {cryptocurrencies.map(crypto => (
             <option
-            key={crypto.CoinInfo.FullName}
-            value={crypto.CoinInfo.Name}
+              key={crypto.CoinInfo.FullName}
+              value={crypto.CoinInfo.Name}
             >{crypto.CoinInfo.FullName}</option>
           ))}
         </select>
