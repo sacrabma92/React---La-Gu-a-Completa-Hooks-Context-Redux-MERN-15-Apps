86ae57a3609d073726d7ee1347f31f51f0ed6356
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-05-03 20:21:53 -0500

    Revert "Varios Cambios"
    This reverts commit 52c37ea8a6246b8274a1b4d1e86ed3058677e61e.

--- 09-Cripto-React-TypeScript/src/components/CryptoPriceDisplay.tsx
+++ 09-Cripto-React-TypeScript/src/components/CryptoPriceDisplay.tsx
@@ -16,7 +16,7 @@ export default function CryptoPriceDisplay() {
           <div className="result">
             <img src={`https://cryptocompare.com/${result.IMAGEURL}`} alt="Imagen Cryptomoneda" />
             <div>
-              <p>El precio es de:: <span>{result.PRICE}</span></p>
+              <p>El precio es de: <span>{result.PRICE}</span></p>
               <p>Precio más alto del día: <span>{result.HIGHDAY}</span></p>
               <p>Precio más bajo del día: <span>{result.LOWDAY}</span></p>
               <p>Variación ultimas 24 horas: <span>{result.CHANGEPCT24HOUR}</span></p>
