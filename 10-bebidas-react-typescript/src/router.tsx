fee26f307d5c8b073b82f21f9c94f9e080f792ce
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-05-11 19:13:38 -0500

    Creando el Layout Principal

--- 10-bebidas-react-typescript/src/router.tsx
+++ 10-bebidas-react-typescript/src/router.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import IndexPage from "./views/IndexPage"
 import FavoritesPage from "./views/FavoritesPage"
+import Layout from "./layouts/Layout"
 
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<IndexPage></IndexPage>} />
-        <Route path='/favoritos' element={<FavoritesPage></FavoritesPage>} />
+        <Route element={<Layout />}>
+          <Route path='/' element={<IndexPage />} index />
+          <Route path='/favoritos' element={<FavoritesPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- 10-bebidas-react-typescript/src/views/FavoritesPage.tsx
+++ 10-bebidas-react-typescript/src/views/FavoritesPage.tsx
@@ -2,8 +2,8 @@
 
 export default function FavoritesPage() {
   return (
-    <div>
-      <h1>Favorite Page</h1>
-    </div>
+    <>
+      <h1>Favorite</h1>
+    </>
   )
 }
