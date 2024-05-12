18b20f71dcb8c6d77f6b9e26120e9b4f40410c40
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-05-12 06:44:08 -0500

    Agregando Navegación entre páginas

--- 10-bebidas-react-typescript/src/components/Header.tsx
+++ 10-bebidas-react-typescript/src/components/Header.tsx
@@ -1,4 +1,4 @@
-
+import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
   return (
@@ -8,6 +8,11 @@ export default function Header() {
           <div>
             <img className="w-32" src="/logo.svg" alt="logotipo" />
           </div>
+
+          <nav className="flex gap-4">
+            <Link to="/" className="text-white uppercase font-bold">Inicio</Link>
+            <Link to="/favoritos" className="text-white uppercase font-bold">Favoritos</Link>
+          </nav>
         </div>
       </div>
     </header>
